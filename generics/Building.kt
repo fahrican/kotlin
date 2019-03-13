a2aedcf03727bd3de822d4f88b5f3b798f124635
Fahri Can <fahri-can@gmx.at>
2019-03-13 14:22:28 +0100

    Update Building.kt

--- generics/Building.kt
+++ generics/Building.kt
@@ -3,7 +3,7 @@ package building
 class Building<T: BaseBuildingMaterial>(val baseBuildingMaterial: T) {
 
     private val baseMaterialsNeeded = 100
-    private val actualMaterialsNeeded = baseMaterialsNeeded * baseBuildingMaterial.numberNeeded
+    val actualMaterialsNeeded = baseMaterialsNeeded * baseBuildingMaterial.numberNeeded
 
     fun build(){
         val material = baseBuildingMaterial::class.simpleName
