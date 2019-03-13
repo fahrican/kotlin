649485bf710e5659ae33fa6184f6e33327d7138c
Fahri Can <fahri-can@gmx.at>
2019-03-13 14:23:11 +0100

    Update App.kt

--- generics/App.kt
+++ generics/App.kt
@@ -6,4 +6,12 @@ fun main() {
     wood.build()
     val brick = Building(Brick())
     brick.build()
+    
+    isSmallBuilding(Building(Wood()))
+    isSmallBuilding(Building(Brick()))
+}
+
+fun <T : BaseBuildingMaterial> isSmallBuilding(building: Building<T>) {
+    if (building.actualMaterialsNeeded < 500) println("Small building")
+    else println("large building")
 }
