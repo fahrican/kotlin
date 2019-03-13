4805bc30ffb9542a21ce96c2a23bc461af16942d
Fahri Can <fahri-can@gmx.at>
2019-03-13 16:12:16 +0100

    Create HigherOrderFunctionsExercise.kt

---
+++ HigherOrderFunctionsExercise.kt
@@ -0,0 +1,25 @@
+package udacity.higher_order_functions
+
+fun main(args: Array<String>) {
+
+    /*
+    Create an extension on 'List' using a higher order function that returns all the numbers in the list that are divisible by 3.
+    Start by creating an extension function on List that takes an lambda on Int and applies it to each item in the list.
+    When the lambda returns zero, include the item in the output. For example, this list:
+     */
+    val numbers = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 0)
+    print(numbers.divisibleBy {
+        it.rem(3)
+    })
+}
+
+fun List<Int>.divisibleBy(block: (Int) -> Int): List<Int> {
+
+    val result = mutableListOf<Int>()
+    for (item in this) {
+        if (block(item) == 0) {
+            result.add(item)
+        }
+    }
+    return result
+}
